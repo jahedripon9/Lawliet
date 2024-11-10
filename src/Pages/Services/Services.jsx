52589cac1de9227f9732ddbdb3eaf523c8be8996
jahedripon9 <jahedahmed3170@gmail.com>
2024-11-10 11:19:11 +0600

    update

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLeaf, faBuilding, faComputer, faGavel } from "@fortawesome/free-solid-svg-icons";
 
